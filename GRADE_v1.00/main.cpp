41a584b0d249e9c629d6493aab7c3386fb473d7a
Farbod Mahmoudinobar <FM59@NJIT.EDU>
2018-12-07 11:58:30 -0500

    Initial Commit

---
+++ GRADE_v1.00/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GRADE_v1.00
+//
+//  Created by Farbod Mahmoudinobar on 12/7/18.
+//  Copyright © 2018 Farbod Mahmoudinobar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
